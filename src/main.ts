f8b1cc6896cbb57587e4de0794d688b01b6f86c0
Hong Jian Wong <hjwongatwork@gmail.com>
2024-10-17 09:04:29 +0800

    Post firebase deployment.

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,64 @@
 /**
  * main.ts
  *
- * Bootstraps Vuetify and other plugins then mounts the App`
+ * Bootstraps Vuetify and other plugins then mounts the App
  */
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-const app = createApp(App)
+// Import Firebase functions
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getAuth } from "firebase/auth"; // Import Authentication
 
-app.directive( 'click-to-expand', {
-    mounted(el, binding) {
-      const cardData = binding.value.cardData; // Get the card data from the binding
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6FnHjsH8aM21IzgRA5lzGOGv2hEeWCH8",
+  authDomain: "gaia-capstone04-prd.firebaseapp.com",
+  projectId: "gaia-capstone04-prd",
+  storageBucket: "gaia-capstone04-prd.appspot.com",
+  messagingSenderId: "1082564059873",
+  appId: "1:1082564059873:web:fc5a9835d2710f88194da0",
+};
 
-      el.addEventListener("click", () => {
-        cardData.showAdditionalText = !cardData.showAdditionalText;
-        cardData.elevation = cardData.showAdditionalText ? 8 : 2;
-      });
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-      document.addEventListener("click", (event) => {
-        if (!el.contains(event.target)) {
-          cardData.showAdditionalText = false;
-          cardData.elevation = 2;
-        }
-      });
-    },
-})
+// Initialize Firestore
+const db = getFirestore(app);
 
-registerPlugins(app)
+// Initialize Authentication
+const auth = getAuth(app);
 
-app.mount('#app')
+const vueApp = createApp(App);
+
+vueApp.directive("click-to-expand", {
+  mounted(el, binding) {
+    const cardData = binding.value.cardData; // Get the card data from the binding
+
+    el.addEventListener("click", () => {
+      cardData.showAdditionalText = !cardData.showAdditionalText;
+      cardData.elevation = cardData.showAdditionalText ? 8 : 2;
+    });
+
+    document.addEventListener("click", (event) => {
+      if (!el.contains(event.target)) {
+        cardData.showAdditionalText = false;
+        cardData.elevation = 2;
+      }
+    });
+  },
+});
+
+registerPlugins(vueApp);
+
+vueApp.mount("#app");
+
+export { db, auth };
